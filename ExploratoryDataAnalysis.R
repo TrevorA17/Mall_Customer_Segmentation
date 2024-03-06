07422e1073e02f61af0be777416996e206aa1a8b
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-03-06 13:02:22 +0300

    Measures of Frequency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -10,3 +10,25 @@ CustomerData <-read.csv("data/Customers.csv", colClasses = c(
 
 #View Dataset
 View(CustomerData)
+
+#Measures of Frequency
+# Display the structure of the dataset
+str(CustomerData)
+
+# Display summary statistics
+summary(CustomerData)
+
+# Frequency table for Gender
+gender_frequency <- table(CustomerData$Gender)
+print("Frequency table for Gender:")
+print(gender_frequency)
+
+# Histogram for Age
+hist(CustomerData$Age, main = "Histogram of Age", xlab = "Age")
+
+# Histogram for Annual Income
+hist(CustomerData$Annual_Income, main = "Histogram of Annual Income", xlab = "Annual Income")
+
+# Histogram for Spending Score
+hist(CustomerData$Spending_Score, main = "Histogram of Spending Score", xlab = "Spending Score")
+
