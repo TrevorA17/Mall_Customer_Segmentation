9777536c7130aa8b5b4ddbc6fd18ff2d9c9c7e84
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-03-06 13:04:01 +0300

    Measures of Central Tendency

--- ExploratoryDataAnalysis.R
+++ ExploratoryDataAnalysis.R
@@ -32,3 +32,34 @@ hist(CustomerData$Annual_Income, main = "Histogram of Annual Income", xlab = "An
 # Histogram for Spending Score
 hist(CustomerData$Spending_Score, main = "Histogram of Spending Score", xlab = "Spending Score")
 
+#Measures of Central Tendency
+# Display mean, median, and mode for Age
+mean_age <- mean(CustomerData$Age)
+median_age <- median(CustomerData$Age)
+mode_age <- names(sort(-table(CustomerData$Age)))[1]
+
+print("Measures of Central Tendency for Age:")
+print(paste("Mean:", mean_age))
+print(paste("Median:", median_age))
+print(paste("Mode:", mode_age))
+
+# Display mean, median, and mode for Annual Income
+mean_income <- mean(CustomerData$Annual_Income)
+median_income <- median(CustomerData$Annual_Income)
+mode_income <- names(sort(-table(CustomerData$Annual_Income)))[1]
+
+print("Measures of Central Tendency for Annual Income:")
+print(paste("Mean:", mean_income))
+print(paste("Median:", median_income))
+print(paste("Mode:", mode_income))
+
+# Display mean, median, and mode for Spending Score
+mean_score <- mean(CustomerData$Spending_Score)
+median_score <- median(CustomerData$Spending_Score)
+mode_score <- names(sort(-table(CustomerData$Spending_Score)))[1]
+
+print("Measures of Central Tendency for Spending Score:")
+print(paste("Mean:", mean_score))
+print(paste("Median:", median_score))
+print(paste("Mode:", mode_score))
+
