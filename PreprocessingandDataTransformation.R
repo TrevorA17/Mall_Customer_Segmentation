103272a7e91ba8d49dc8a6a51180083ecef6f20f
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-03-06 13:18:13 +0300

    Check for missing values

--- PreprocessingandDataTransformation.R
+++ PreprocessingandDataTransformation.R
@@ -10,3 +10,16 @@ CustomerData <-read.csv("data/Customers.csv", colClasses = c(
 
 #View Dataset
 View(CustomerData)
+
+# Check for missing values
+missing_values <- sum(is.na(CustomerData))
+
+# Display confirmation
+if (missing_values > 0) {
+  print("Missing values are present in the dataset.")
+  print(paste("Total missing values: ", missing_values))
+  print("Columns with missing values:")
+  print(names(CustomerData)[colSums(is.na(CustomerData)) > 0])
+} else {
+  print("No missing values are present in the dataset.")
+}
