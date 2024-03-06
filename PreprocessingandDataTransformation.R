ac64ad27ae958defb2c3f5f0c3381c45caea3083
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-03-06 13:19:50 +0300

    Data transformations

--- PreprocessingandDataTransformation.R
+++ PreprocessingandDataTransformation.R
@@ -23,3 +23,15 @@ if (missing_values > 0) {
 } else {
   print("No missing values are present in the dataset.")
 }
+
+# Data transformations
+
+# Standardizing numeric variables (Age, Annual_Income, Spending_Score)
+numeric_cols <- c("Age", "Annual_Income", "Spending_Score")
+CustomerData[numeric_cols] <- scale(CustomerData[numeric_cols])
+
+# Creating a new feature (Age category)
+CustomerData$Age_Category <- cut(CustomerData$Age, breaks = c(0, 30, 50, Inf), labels = c("Young", "Middle-aged", "Senior"))
+
+# Display the transformed dataset
+head(CustomerData)
