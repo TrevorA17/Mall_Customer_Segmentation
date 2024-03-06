047e87fab5085a9d2c0b34aaca51b5d15519111f
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-03-06 12:56:22 +0300

    CSV update

---
+++ ExploratoryDataAnalysis.R
@@ -0,0 +1,7 @@
+CustomerData <-read.csv("data/Customers.csv", colClasses = c(
+  CustomerId = "numeric",
+  Gender = "factor",
+  Age = "numeric",
+  Annual_Income = "numeric",
+  Spending_Score = "numeric"
+))
