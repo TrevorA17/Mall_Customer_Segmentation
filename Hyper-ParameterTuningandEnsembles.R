b5ab2fae3c3c8dfa984b8115c588c5e4a6c5f580
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-03-06 13:42:24 +0300

    load dataset for hyper-parameter tuning and ensembles

---
+++ Hyper-ParameterTuningandEnsembles.R
@@ -0,0 +1,12 @@
+#Load Dataset
+
+CustomerData <-read.csv("data/Customers.csv", colClasses = c(
+  CustomerId = "numeric",
+  Gender = "factor",
+  Age = "numeric",
+  Annual_Income = "numeric",
+  Spending_Score = "numeric"
+))
+
+#View Dataset
+View(CustomerData)
